9c5155488e35910481145e29f0cff3a52c116f95
clasp <1093230325@qq.com>
2025-07-16 16:17:38 +0800

    Update l4d2.js

--- web/static/js/l4d2.js
+++ web/static/js/l4d2.js
@@ -576,8 +576,8 @@ var app = new Vue({
             }
         },
         async uploadByChunk(fileitem) {
-            const CHUNK_SIZE = 1 * 1024 * 1024;
-            const MAX_CONCURRENT_UPLOADS = 2;
+            const CHUNK_SIZE = 5 * 1024 * 1024;
+            const MAX_CONCURRENT_UPLOADS = 5;
             const MAX_RETRY_COUNT = 3;
             let retryBtn = $(`[data-filename="${fileitem.fileName}"]`).find('.btn-secondary');
             this.updateProgress(fileitem.fileName, 0, 'uploading');
